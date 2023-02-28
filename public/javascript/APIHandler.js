74374f09e42c7652393865e1d54ff7e7f2ddce7a
Gonçalo Vilaboim <gonvipa@gmail.com>
2023-02-28 21:56:29 +0000

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,26 +1,32 @@
+
 class APIHandler {
   constructor(baseUrl) {
     this.BASE_URL = baseUrl
-    this.api = axios.create(this.BASE_URL)
   }
 
-  getFullList() {
-    return this.api.get('/characters')
+  async getFullList () {
+    const response = await axios.get(`${this.BASE_URL}/characters`);
+    return response.data;
   }
 
-  getOneRegister() {
-    return this.api.get(`/characters/${characterId}`)
+  async getOneRegister(characterId) {
+  
+    const response =  await axios.get(`${this.BASE_URL}/characters/${characterId}`)
+    return response.data;
   }
 
-  createOneRegister() {
-    return this.api.post(`/characters`, characterInfo)
+  async createOneRegister(characterInfo) {
+    const response =  await axios.post(`${this.BASE_URL}/characters/`, characterInfo)
+    return response.data
   }
 
-  updateOneRegister() {
-    return this.api.put(`/characters/${characterId}`, characterInfo)
+  async updateOneRegister(characterId ,characterInfo) {   
+    const response =  await axios.put(`${this.BASE_URL}/characters/${characterId}`, characterInfo)
+    return response.data
+
   }
 
-  deleteOneRegister() {
-    return this.api.delete(`/characters/${characterId}`)
+  async deleteOneRegister(characterId) {
+    const response =  await axios.delete(`${this.BASE_URL}/characters/${characterId}`)
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,105 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList().then(characters => {
+      console.log(characters);
+      const mainDiv = document.querySelector(".characters-container")
+      mainDiv.innerHTML = "";
+      characters.forEach(element => {
+        let div = document.createElement('div')
+        div.classList.add("character-info");
+        div.innerHTML =`
+        <div class="name">Character :${element.name}</div>
+        <div class="occupation">Character occupation: ${element.occupation}</div>
+        <div class="cartoon">Is a Cartoon?  ${element.cartoon}</div>
+        <div class="weapon">Character weapon :  ${element.weapon}</div>
+        `;
+        mainDiv.appendChild(div);
+      });
+    
+
+    }).catch(error => {
+      console.log(error)
+    })
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    let id = document.getElementById('character-id')
+    const mainDiv = document.querySelector(".characters-container")
+    mainDiv.innerHTML = "";
+   charactersAPI.getOneRegister(id.value).then(element => {
+    let div = document.createElement('div')
+    div.classList.add("character-info");
+    div.innerHTML =`
+    <div class="name">Character :${element.name}</div>
+    <div class="occupation">Character occupation: ${element.occupation}</div>
+    <div class="cartoon">Is a Cartoon?  ${element.cartoon}</div>
+    <div class="weapon">Character weapon :  ${element.weapon}</div>
+    `;
+    mainDiv.appendChild(div);
+
+   }).catch(error => {
+    console.log(error)
+  })
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    let id = document.getElementById('character-id-delete')
+    charactersAPI.deleteOneRegister(id.value).then(element => { 
+    document.getElementById('delete-one').classList.add('active');
+    }).catch(error => {
+      console.log(error)
+    })
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let id = document.getElementById('id').value
+    let name = document.getElementById('name').value
+    let occupation = document.getElementById('occupation').value
+    let weapon = document.getElementById('weapon').value
+    let cartoon = document.getElementById('cartoon').checked
+
+    let obj = {
+      id : id,
+      name : name,
+      occupation : occupation,
+      weapon: weapon,
+      cartoon : cartoon
+    }
+    
+    charactersAPI.updateOneRegister(id , obj).then(element => { 
+      console.log(element)
+      }).catch(error => {
+        console.log(error)
+      })
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+    event.preventDefault();
+    let name = document.getElementById('create-name').value
+    let occupation = document.getElementById('create-occupation').value
+    let weapon = document.getElementById('create-weapon').value
+    let cartoon = document.getElementById('create-cartoon').checked
+
+    let obj = {
+      name : name,
+      occupation : occupation,
+      weapon: weapon,
+      cartoon : cartoon
+    }
+    
+    charactersAPI.createOneRegister(obj).then(element => { 
+      console.log(element)
+      }).catch(error => {
+        console.log(error)
+      })
+
 
   });
 });
